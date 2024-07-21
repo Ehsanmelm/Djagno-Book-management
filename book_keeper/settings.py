e6f701b2dfe4809510fcea3c6ada84c7f49d2ea9
Ehsan <ehsanmelm82@gmail.com>
2024-07-21 15:02:30 +0330

    faet : setting up postgres sql

--- book_keeper/settings.py
+++ book_keeper/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'book_keeper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'book_manage', 
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
